bd8ab4a4bab463d5a220a1f23b75da95503ae066
Yan Linfeng <yan.linfeng@foxmail.com>
2023-06-25 18:06:06 +0800

    commit

---
+++ src/main/java/org/example/GetTest.java
@@ -0,0 +1,149 @@
+package org.example;
+
+import org.apache.jmeter.assertions.ResponseAssertion;
+import org.apache.jmeter.config.Arguments;
+import org.apache.jmeter.config.gui.ArgumentsPanel;
+import org.apache.jmeter.control.LoopController;
+import org.apache.jmeter.control.gui.LoopControlPanel;
+import org.apache.jmeter.engine.StandardJMeterEngine;
+import org.apache.jmeter.extractor.json.jsonpath.JSONPostProcessor;
+import org.apache.jmeter.extractor.json.jsonpath.gui.JSONPostProcessorGui;
+import org.apache.jmeter.protocol.http.control.Header;
+import org.apache.jmeter.protocol.http.control.HeaderManager;
+import org.apache.jmeter.protocol.http.control.gui.HttpTestSampleGui;
+import org.apache.jmeter.protocol.http.gui.HeaderPanel;
+import org.apache.jmeter.protocol.http.sampler.HTTPSampler;
+import org.apache.jmeter.protocol.http.sampler.HTTPSamplerProxy;
+import org.apache.jmeter.protocol.http.util.HTTPFileArg;
+import org.apache.jmeter.reporters.ResultCollector;
+import org.apache.jmeter.reporters.Summariser;
+import org.apache.jmeter.samplers.SampleSaveConfiguration;
+import org.apache.jmeter.testelement.TestElement;
+import org.apache.jmeter.testelement.TestPlan;
+import org.apache.jmeter.threads.ThreadGroup;
+import org.apache.jmeter.util.JMeterUtils;
+import org.apache.jorphan.collections.HashTree;
+import org.apache.jorphan.collections.ListedHashTree;
+import picocli.CommandLine;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Random;
+import java.util.concurrent.Callable;
+
+@CommandLine.Command(name = "gettest", mixinStandardHelpOptions = true, description = "Execute Coherence Put Test.")
+public class GetTest implements Callable<Integer> {
+    @CommandLine.Option(names = {"-jmeterHome", "--jmeterHome"}, required = true, description = "JMeter's Home directory.")
+    private String jmeterHome;
+    @CommandLine.Option(names = {"-numThreads", "--numThreads"}, defaultValue = "10", description = "Parallel threads number you want to specify. (Like how many users)")
+    private int numThreads;
+    @CommandLine.Option(names = {"-hosts", "--hosts"}, split=",", defaultValue = "127.0.0.1", description = "The coherence rest proxy hosts. You can specicy multiple hosts seperated by [,] sign. eg 192.168.0.1,192.168.0.2")
+    private String[] hosts;
+    @CommandLine.Option(names = {"-port", "--port"}, defaultValue = "8080", description = "The port number of coherence rest proxy. Default is 8080.")
+    private int port;
+
+    public static void main(String[] args) throws IOException {
+        int exitCode = new CommandLine(new GetTest()).execute(args);
+        System.exit(exitCode);
+    }
+
+    @Override
+    public Integer call() throws Exception {
+        // Create the Jmeter engine
+        StandardJMeterEngine jm = new StandardJMeterEngine();
+
+        // Configure JMeter properties
+        JMeterUtils.setJMeterHome(jmeterHome);
+        JMeterUtils.loadJMeterProperties(jmeterHome+"/bin/jmeter.properties");
+        JMeterUtils.initLocale();
+
+        // Response Assertion
+        ResponseAssertion responseAssertion = new ResponseAssertion();
+        responseAssertion.setTestFieldResponseCode();
+        responseAssertion.addTestString("200");
+
+
+        // Create a loop controller
+        LoopController loopController = new LoopController();
+        loopController.setLoops(1);
+        loopController.setFirst(true);
+        loopController.setProperty(TestElement.TEST_CLASS, LoopController.class.getName());
+        loopController.setProperty(TestElement.GUI_CLASS, LoopControlPanel.class.getName());
+        loopController.initialize();
+
+        // Create a thread group
+        ThreadGroup threadGroup = new ThreadGroup();
+        threadGroup.setName("Default Thread Group");
+        threadGroup.setNumThreads(numThreads);
+        threadGroup.setRampUp(1);
+        threadGroup.setSamplerController(loopController);
+        threadGroup.setProperty(TestElement.TEST_CLASS, ThreadGroup.class.getName());
+
+        // Create a test plan
+        TestPlan testPlan = new TestPlan("Coherence Get Test Plan");
+        testPlan.setProperty(TestElement.TEST_CLASS, TestPlan.class.getName());
+        testPlan.setUserDefinedVariables((Arguments) new ArgumentsPanel().createTestElement());
+
+        // Create a new hash tree to hold our test elements
+        ListedHashTree testPlanTree = new ListedHashTree();
+        // Add the test plan to our hash tree, this is the top level of our test
+        testPlanTree.add(testPlan);
+
+        // Create another hash tree and add the thread group to our test plan
+        HashTree threadGroupHashTree = testPlanTree.add(testPlan, threadGroup);
+
+        File testdataDir = new File("./testdata");
+        File[] files = testdataDir.listFiles();
+        for(File jsonFile:files){
+            System.out.println(jsonFile.getName());
+            // Create a sampler
+            HTTPSamplerProxy getHttpSampler = new HTTPSamplerProxy();
+            getHttpSampler.setDomain(hosts[new Random().nextInt(hosts.length)]);
+            getHttpSampler.setPort(port);
+            getHttpSampler.setPath("/cache/json/"+jsonFile.getName());
+            getHttpSampler.setMethod("GET");
+            getHttpSampler.setProtocol("http");
+            getHttpSampler.setProperty(TestElement.TEST_CLASS, HTTPSampler.class.getName());
+            getHttpSampler.setProperty(TestElement.GUI_CLASS, HttpTestSampleGui.class.getName());
+
+            // Create a hash tree to add the post processor to
+            HashTree httpSamplerTree = new HashTree();
+            httpSamplerTree.add(getHttpSampler, responseAssertion);
+
+            // Add the http sampler to the hash tree that contains the thread group
+            threadGroupHashTree.add(httpSamplerTree);
+        }
+
+        // Summariser
+        Summariser summariser = null;
+        String summariserName = JMeterUtils.getPropDefault("summarise.names", "summary response");
+        if (summariserName.length() > 0) {
+            summariser = new Summariser(summariserName);
+        }
+
+        ResultCollector logger = new ResultCollector(summariser);
+        testPlanTree.add(testPlanTree.getArray()[0], logger);
+
+        // Write to a file
+        ResultCollector rc = new ResultCollector();
+        rc.setEnabled(true);
+        rc.setErrorLogging(false);
+        rc.isSampleWanted(true);
+        SampleSaveConfiguration ssc = new SampleSaveConfiguration();
+        ssc.setTime(true);
+        ssc.setAssertionResultsFailureMessage(true);
+        ssc.setThreadCounts(true);
+        rc.setSaveConfig(ssc);
+        rc.setFilename("./CoherenceGetTestResults.jtl");
+        testPlanTree.add(testPlanTree.getArray()[0], rc);
+
+        // Configure
+        jm.configure(testPlanTree);
+
+        // Run
+        jm.run();
+
+        return 0;
+    }
+
+}
---
+++ src/main/java/org/example/TestDataGenerator.java
@@ -0,0 +1,110 @@
+package org.example;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+import java.util.concurrent.Callable;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@Command(name = "generate", mixinStandardHelpOptions = true, description = "Generate Json data files.")
+public class TestDataGenerator implements Callable<Integer> {
+    private static final Pattern REGEXP_JSON_SIZE = Pattern.compile("^([0-9]+)(MB|KB|B|)$");
+    private static final String[] firstNames = new String[]{"John", "James", "Robert", "Michael", "William", "David", "Richard", "Charles", "Joseph", "Thomas", "Christopher", "Daniel", "Paul", "Mark", "Donald", "George", "Kenneth", "Steven", "Edward", "Brian", "Ronald", "Anthony", "Kevin", "Jason", "Matthew", "Gary", "Timothy", "Jose", "Larry", "Jeffrey", "Frank", "Scott", "Eric", "Stephen", "Andrew", "Raymond", "Gregory", "Joshua", "Jerry", "Dennis", "Walter", "Patrick", "Peter", "Harold", "Douglas", "Henry", "Carl", "Arthur", "Ryan", "Roger"};
+    private static final String[] lastNames = new String[]{ "Smith",  "Johnson",  "Williams",  "Brown",  "Jones",  "Garcia",  "Rodriguez",  "Wilson",  "Martinez",  "Anderson",   "Taylor",  "Thomas",  "Hernandez",  "Moore",  "Martin",  "Jackson",  "Thompson",  "White",  "Lopez",  "Lee", "Gonzalez",  "Harris",  "Clark",  "Lewis",  "Robinson",  "Walker",  "Perez",  "Hall",  "Young",  "Allen",  "Sanchez",  "Wright",  "King",  "Scott",  "Green",  "Adams",  "Baker",  "Gomez",  "Nelson",  "Hill",   "Ramirez",  "Campbell",  "Mitchell",  "Roberts",  "Carter",  "Phillips",  "Evans",  "Turner",  "Torres",  "Parker",  "Collins",  "Edwards",  "Stewart",  "Flores",  "Morris",  "Nguyen",  "Murphy",  "Rivera",  "Cook",  "Rogers",  "Morgan",  "Peterson",  "Cooper",  "Reed",  "Bailey",  "Bell",  "Gutierrez",  "Kelly",  "Howard",  "Ward",  "Cox",  "Diaz",  "Richardson",  "Wood",  "Watson",  "Brooks",  "Bennett",  "Gray",  "James",  "Reyes",  "Cruz",  "Hughes",  "Price",  "Myers",  "Long",  "Foster",  "Sanders",  "Ross",  "Morales",  "Powell",  "Sullivan",  "Russell",  "Ortiz",  "Jenkins",  "Perry",  "Butler",  "Coleman",  "Simmons",  "Patterson",  "Jordan",  "Reynolds",  "Hamilton",  "Graham",  "Kim",  "Davis"};
+    private static final String[] hobbies = new String[]{ "Reading", "Traveling", "Cycling", "Cooking", "Photography", "Writing", "Painting", "Movies", "Swimming", "Social networking", "Gaming", "Outdoor sports", "Music", "Gardening", "Coffee", "Films", "Hiking", "Board games", "Driving", "Shopping", "Pet care", "Learning languages", "Tennis", "Fishing", "Travel", "Dancing", "Painting   ", "Gadgets", "Cooking", "Fitness", "Yoga", "Blogging", "Stamp collecting", "Watching sports", "Finance and investment", "Card games", "Baking", "Camping", "Reading comics","Car repairing"};
+    private static final String[] genders = new String[]{"Male","Female"};
+    private static final char[] chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789".toCharArray();
+    @Option(names = {"-c", "--count"}, description = "The number of json files you want to generate.")
+    private int count = 100;
+    @Option(names = {"-size", "--size"}, description = "The json file size you want to generate. B, KB, MB is available to specify, the max size you can specify is 32MB.")
+    private String size = "10KB";
+    private int JSON_SIZE_IN_BYTES = 100;
+
+    @Option(names = {"-start", "--start-id"}, description = "The start id you can specify for your json.")
+    private int start = 1;
+
+    @Override
+    public Integer call() throws Exception {
+        this.JSON_SIZE_IN_BYTES = parseJSONSize(size);
+        for(int i=start;i<start+count;i++){
+            String filename = String.format("%s.json",i);
+            Map<String,Object> entity = new HashMap();
+            entity.put("id",i);
+            entity.put("name",generateName());
+            entity.put("age", new Random().nextInt(60)+18);
+            entity.put("gender",genders[new Random().nextInt(2)]);
+            entity.put("hobby",hobbies[new Random().nextInt(hobbies.length)]);
+            entity.put("comment","");
+            entity.put("comment",generateComment(entity));
+            ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper.writeValue(new File("./testdata/"+filename),entity);
+        }
+        return 0;
+    }
+
+    public static void main(String... args) {
+        int exitCode = new CommandLine(new TestDataGenerator()).execute(args);
+        System.exit(exitCode);
+    }
+
+    private String generateComment(Map<String,Object> entity) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        int docLengthWithoutComment =objectMapper.writeValueAsString(entity).getBytes().length;
+        if(docLengthWithoutComment >= JSON_SIZE_IN_BYTES){
+            return "";
+        }else{
+            int commentLength = JSON_SIZE_IN_BYTES - docLengthWithoutComment;
+            return randomAlpha(commentLength);
+        }
+    }
+
+    private static String randomAlpha(int length){
+        StringBuilder stringBuilder = new StringBuilder("");
+        for(int i=0;i<length;i++){
+            stringBuilder.append(chars[new Random().nextInt(chars.length)]);
+        }
+        return stringBuilder.toString();
+    }
+
+    private static String generateName() {
+
+        Random random = new Random();
+        int firstNameIndex = random.nextInt(firstNames.length);
+        int lastNameIndex = random.nextInt(lastNames.length);
+
+        return firstNames[firstNameIndex] + " " + lastNames[lastNameIndex];
+    }
+
+    private int parseJSONSize(Object json_size) {
+        if(json_size == null || !(json_size instanceof String)){
+            return 0;
+        }
+        String jsonSize = json_size.toString().toUpperCase();
+        Matcher matcher = REGEXP_JSON_SIZE.matcher(jsonSize);
+        if(matcher.matches()){
+            String intPart = matcher.group(1);
+            String unit = matcher.group(2);
+            int byteCount = Integer.parseInt(intPart);
+            switch (unit){
+                case "KB":
+                    byteCount *= 1024;
+                    break;
+                case "MB":
+                    byteCount *= 1024*1024;
+                    break;
+            }
+            return byteCount;
+        }else{
+            return 0;
+        }
+    }
+
+}
